9ab43e993c131157b0cec02fe59695367f6aeaa5
munHunger <marcusmunger@hotmail.com>
2016-08-13 18:02:15 +0200

    First style commit

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<html>
+<!DOCTYPE html>
 <head>
+<link rel="icon" href="http://munhunger.se/images/icon.ico">
+<title>Munhunger</title>
 </head>
-<body>
-Hello World!
+<body style="background-color:#0F0F0F">
+	<div style="position:relative; width:640px; margin-left:auto; margin-right:auto; margin-top:150px; background-color:#151515; border-radius:15px">
+		
+	</div>
 </body>
 </html>
